a4137da356a11cfc28260a97dc91bcf74f97f9b7
Aritra Ghorai <81803365+aritraghorai@users.noreply.github.com>
2023-08-12 18:38:50 +0530

    change vim config

--- dot_config/nvim/after/plugin/whichKey.lua
+++ dot_config/nvim/after/plugin/whichKey.lua
@@ -84,8 +84,7 @@ local mappings = {
         "Buffers",
     },
     ["e"] = { "<cmd>NvimTreeToggle<cr>", "Explorer" },
-    ["q"] = { "<cmd>q!<CR>", "Quit" },
-    ["c"] = { "<cmd>Bdelete!<CR>", "Close Buffer" },
+    ["q"] = { "<cmd>Bdelete!<CR>", "Close Buffer" },
     ["h"] = { "<cmd>nohlsearch<CR>", "No Highlight" },
     ["f"] = {
         "<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown{previewer = false})<cr>",
