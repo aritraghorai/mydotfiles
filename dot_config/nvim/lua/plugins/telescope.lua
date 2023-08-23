36aa8c21a4315e78d8594dff92b758f9918a9bfc
Aritra Ghorai <81803365+aritraghorai@users.noreply.github.com>
2023-08-23 23:29:42 +0530

    Added new astrovim

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -1,21 +1,42 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    keys = {
-      {
-        "<leader>gb",
-        function()
-          require("telescope.builtin").git_branches()
-        end,
-        desc = "Find git branch",
-      },
-      {
-        "<c-p>",
-        function()
-          require("telescope.builtin").git_files()
-        end,
-        desc = "Find All Files in it",
-      },
-    },
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    { "nvim-telescope/telescope-fzf-native.nvim", enabled = vim.fn.executable "make" == 1, build = "make" },
   },
+  cmd = "Telescope",
+  opts = function()
+    local actions = require "telescope.actions"
+    local get_icon = require("astronvim.utils").get_icon
+    return {
+      defaults = {
+        git_worktrees = vim.g.git_worktrees,
+        prompt_prefix = get_icon("Selected", 1),
+        selection_caret = get_icon("Selected", 1),
+        path_display = { "truncate" },
+        sorting_strategy = "ascending",
+        layout_config = {
+          horizontal = { prompt_position = "top", preview_width = 0.55 },
+          vertical = { mirror = false },
+          width = 0.87,
+          height = 0.80,
+          preview_cutoff = 120,
+        },
+        mappings = {
+          i = {
+            ["<C-n>"] = actions.cycle_history_next,
+            ["<C-p>"] = actions.cycle_history_prev,
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-k>"] = actions.move_selection_previous,
+          },
+          n = { q = actions.close },
+        },
+      },
+    }
+  end,
+  config = require "plugins.configs.telescope",
+  keys = { {
+    "<leader><leader>",
+    "<cmd>Telescope git_files<cr>",
+    desc = "Open All git files",
+  } },
 }
