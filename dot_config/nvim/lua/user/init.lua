f2dc76e0ecadf36b467adca7ec357e98c437d3fd
Aritra Ghorai <81803365+aritraghorai@users.noreply.github.com>
2023-09-17 11:19:30 +0530

    added graphql assignment

--- dot_config/nvim/lua/user/init.lua
+++ dot_config/nvim/lua/user/init.lua
@@ -18,7 +18,10 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  -- colorscheme = "astrodark",
+  colorscheme = "vscode",
+  -- colorscheme = "sonokai",
+  -- colorscheme = "github_dark",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
